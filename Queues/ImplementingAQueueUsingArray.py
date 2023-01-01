ca2f3682bccdc26a46969015c5b014dc9028f337
Daniella Omokore <daniellaomokore@gmail.com>
2023-01-01 16:01:15 +0000

    commit

---
+++ Queues/ImplementingAQueueUsingArray.py
@@ -0,0 +1,106 @@
+"""
+Implementing a Queue Data structure using an Array
+
+The value of front represents the index number for the 'front' element in the queue. If your queue has 3 elements in it
+[5,33,8], the value of front will be 0, as index 0 represents the index for the top element-which has the value of '5' .
+The front value is constant and will ALWAYS be 0 as 0 index is the first element in an array.
+
+
+push - increase rear pointer index by 1, increase size count by 1,  add your element to the rear of the queue.
+pop - decrease rear pointer index by 1, decrease size count by 1, remove an element from the front of the queue.
+peek - returns the actual value of element that the 'front' pointer is pointing to (so not it's index, the actual element).
+
+
+"""
+
+class Queue:
+
+    # INITIALISING ALL THE RULES/FEATURES OF A STACK
+    def __init__(self, capacity):   # remember to get user input for the capacity of the queue
+        self.queue = []  # empty queue
+        self.front = 0  # front points to the front element of the queue, so will always be at index 0 of an array
+        self.rear = -1  # rear points to the index of the last element of the queue, so it's -1 here as the array is empty
+        self.capacity = capacity   # queues have a pre-defined maximum capacity
+        self.count = 0  #  initialise the size of the queue to 0 as it's empty, increase by 1 when pushing and decrease by 1 when popping
+
+    # returns the size of the queue
+    # size of the queue will be the pointer TOP value + 1
+    def queue_size(self):
+        return self.count
+
+    # the queue is full if the size of the queue == the capacity of the queue
+    def queue_is_full(self):
+        return self.queue_size() == self.capacity    # this returns true if queue is full
+
+    # the queue is empty if the size of the queue == 0
+    def queue_is_empty(self):
+        return self.queue_size() == 0    # this returns true if queue is empty
+
+    # ADDS ITEM TO REAR OF QUEUE
+    def push(self, value):
+        # check if queue is full
+        if self.queue_is_full():   # if queue is full
+            return "Sorry you can't add any items to the queue, you've reached maximum capacity."
+
+        else:  # if the queue isn't full, continue the process to push the item
+
+            self.count += 1   # increase count by 1
+            self.queue.append(value)  # then next you can add the value to the rear of your queue
+            self.rear += 1   # increase rear index by 1
+
+            return "{} has been pushed to your queue".format(value)
+
+    # REMOVES ITEM FROM FRONT OF QUEUE
+    def pop(self):
+        # check if queue is empty
+        if self.queue_is_empty(): # if queue is empty
+            return "Sorry, You can't remove an item as your queue is already empty."
+
+        else:       # if your queue isn't empty, continue the process to pop an item
+
+            self.queue.pop(self.front)    # remove the front value from the queue
+            self.count -= 1       # decrease count by 1
+            self.rear -= 1   # decrease rear index by 1
+
+            return "An item has been popped from your queue"
+
+    # RETURNS THE 'FRONT' POINTER VALUE OF THE QUEUE - SO RETURNS THE ACTUAL VALUE OF THE FRONT ITEM OF THE QUEUE (NOT IT'S INDEX]
+    def peek(self):
+
+        if self.queue_is_empty(): # if queue is empty
+            return "Sorry, You can't get the top/peek value as your queue is empty."
+
+        else:
+            top_value = self.queue[self.front]
+            return top_value
+
+    # PRINTS THE ENTIRE STACK
+    def print_queue(self):
+        return self.queue
+
+
+
+# Driver Code
+
+yellasQueue = Queue(3) # [none,none,none]   It will start with a capacity of 6, and size of 0
+
+print(yellasQueue.queue_is_empty())   # returns True as the queue is empty
+print(yellasQueue.queue_is_full())  # returns False as the queue is not full
+
+
+print(yellasQueue.push(6))  #[6,none,none]   capacity of 3, size of 1
+print(yellasQueue.push(7))  #[6,7,none]      capacity of 3, size of 2
+print(yellasQueue.push(7))  #[6,7,7]         capacity of 3, size of 3
+print(yellasQueue.peek())   # returns peek value of 6
+
+print(yellasQueue.queue_is_full())  # returns True as the queue is full
+print(yellasQueue.queue_is_empty())  # returns False as the queue is not empty
+
+
+print(yellasQueue.print_queue()) #  [6,7,7]         capacity of 3, size of 3
+print(yellasQueue.push(6))
+print(yellasQueue.pop())         # [6,7,none]      capacity fof 3, size of 2
+print(yellasQueue.print_queue()) # [6,7,none]      capacity fof 3, size of 2
+print(yellasQueue.queue_size())   # returns queue size of 2
+
+
---
+++ Stacks/ImplementingAStackUsingALinkedList.py
@@ -0,0 +1,127 @@
+"""
+Implementing a Queue Data structure using a Linked List
+
+Top = is the pointer that points to the index of the very top value of a queue.
+'Head' refers to Top in the case of Linked Lists.
+
+The value of top represents the 'top' node in the queue. If your queue has 3 elements in it [5,33,8], the value of top will be '8'.
+
+If your queue is empty, the value of top will be 'none'.
+
+push - create a new node, assign the newNode's 'next' node to point to the current 'Top' node , re-assign newNode as
+the current 'Top' node.
+pop - decrease 'Top' (index) by 1, remove an element from the top of the queue.
+peek - returns the actual value of element that the 'Top' pointer is pointing to  (so not it's index, the actual element).
+
+LinkedList stacks technically can't be full so we leave that function out
+
+"""
+
+class Node:
+
+    def __init__(self,data):
+        self.data = data
+        self.next = None  # When you initialise a node at first it's pointer isn't pointing to anything, you decide that while making the LL
+
+class Stack:
+
+    # INITIALISING ALL THE RULES/FEATURES OF A STACK
+    def __init__(self):   # remember to get user input for the capacity of the queue
+        self.top = None  # the value of 'top' is initially set to 'None' as we are starting with an empty queue
+        self.count = 0  # count variable to track the size of the queue , it's initally 0 as we start with an empty queue
+
+    # returns the size of the queue
+    # size of the queue will be the pointer TOP value + 1
+    def stack_size(self):
+        return self.count
+
+    # the queue is empty if the size of the queue == 0
+    def stack_is_empty(self):
+        # return self.count == 0
+        if self.stack_size() == 0  or self.count == 0 or self.top == None:   # if queue is empty
+            return True
+        else:
+            return False
+
+
+    # ADDS ITEM TO TOP OF STACK
+    def push(self, value):
+        # check if queue is full
+        if self.stack_is_empty():   # if queue is empty
+             self.top = Node(value)  # initialise a new node and set the 'Top' pointer to it
+             self.count += 1   # increase the size count of the linked list by 1
+
+        else:  # if the queue isn't empty
+
+            newNode = Node(value)  # initialise a new node
+            newNode.next = self.top # assign the node directly next to the 'newNode' to be pointing to the current 'Top' node
+            self.top = newNode      # reassign the 'newNode' as current 'Top' node, since now the 'newNode' is one
+            # ... node before the previous 'Top' node so the pointer needs to be updated.
+            self.count += 1  # increase the size count of the linked list by 1
+
+        return "{} has been pushed to your queue".format(value)
+
+
+    # REMOVES ITEM FROM TOP OF STACK
+    def pop(self):
+        # check if queue is empty
+        if self.stack_is_empty(): # if queue is empty
+            return "Sorry, You can't remove an item as your queue is already empty."
+        else:       # if your queue isn't empty, continue the process to pop an item
+
+            poppedNode = self.top  # Assign 'poppedNode' to the current 'Top' Node of the LL
+            self.top = self.top.next  # Make the 'Top' node pointer point to the node after the current 'Top' node -
+            # ... so basically making the next node the 'Top' of the LL instead
+            self.count -= 1         # decrease the size by one
+            poppedNode.next = None  # since the node is being deleted, make it's next pointer be equal to nothing
+            return "An item has been popped from your queue"
+
+    # RETURNS THE 'TOP' ELEMENT OF THE STACK
+    def peek(self):
+
+        if self.stack_is_empty():  # if the queue is empty
+            return "You can't get to top/peek value as the queue is empty"
+        else:    # if not empty
+            top_value = self.top.data
+            return top_value
+
+    # PRINTS THE ENTIRE STACK
+    def print_stack(self):
+
+        iternode = self.top
+        if self.stack_is_empty():
+            return "Queue Underflow"
+
+        else:
+
+            while (iternode != None):
+
+                print(iternode.data, end="")
+                iternode = iternode.next
+                if (iternode != None):
+                    print(" -> ", end="")
+
+            return "\nQueue Has Been Printed Above"
+
+
+
+# Driver Code
+
+yellasStack = Stack() # [none,none,none]   It will start with a capacity of 6, and size of 0
+
+print(yellasStack.stack_is_empty())   # returns True as the queue is empty
+
+
+print(yellasStack.push(6))  # [6,none,none]   capacity of 3, size of 1
+print(yellasStack.push(7))  # [6,7,none]      capacity of 3, size of 2
+print(yellasStack.push(7))  # [6,7,7]         capacity of 3, size of 3
+print(yellasStack.peek())   # returns peek value of 7
+
+print(yellasStack.stack_is_empty())  # returns False as the queue is not empty
+
+
+print(yellasStack.print_stack()) #  [6,7,7]         capacity of 3, size of 3
+print(yellasStack.pop())         # [6,7,none]      capacity fof 3, size of 2
+print(yellasStack.print_stack()) # [6,7,none]      capacity fof 3, size of 2
+print(yellasStack.stack_size())   # returns queue size of 2
+
