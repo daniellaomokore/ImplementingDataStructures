30c67c709560ad74c16bf28fe828e38f9174020d
Daniella Omokore <daniellaomokore@gmail.com>
2023-01-05 20:58:52 +0000

    commit

--- BinaryTrees/ImplementingABinaryTree.py
+++ BinaryTrees/ImplementingABinaryTree.py
@@ -4,12 +4,14 @@ Implementing a Binary (search) Tree
 
 OPERATIONS:
 
--Insert - compares the node value to the parent node and decided whether to add it as a left node or right node. Rule:the left node must be smaller than the root node
-If the node is greater than the parent node it is inserted as a right node, otherwise it's inserted left.
+-Insert - compares the node value to the parent node and decided whether to add it as a left node or right node.
+Rule:the left node must be smaller than the root node If the node is greater than the parent node it is inserted as a
+right node, otherwise it's inserted left.
 
 -Size - returns the size of the linked list
 
--find_value - this check if a value exists in the tree or not. When searching for a node in a tree, we transverse the node from left to right with a parent.
+-find_value - this check if a value exists in the tree or not. When searching for a node in a tree, we transverse the
+node from left to right with a parent.
 
 - print_tree - prints the tree values
 """
@@ -30,15 +32,17 @@ class Node:
         if self.data: # if there is a parent node
             if insert_node < self.data:  # if the node you want to insert is smaller than the current node
                 if self.left is None:  # if the left child of that node is empty
-                    self.left = Node(insert_node) # insert your node there
+                    self.left = Node(insert_node)  # insert your node there
                 else:   # else if the left child of the node already has a value
-                    self.left.insert(insert_node) # carry out the same operation but to that left child node - basically moving down the left side of the tree
+                    self.left.insert(insert_node)  # carry out the same operation but to that left child node -
+                    # basically moving down the left side of the tree
             elif insert_node > self.data: # else if the node you want to insert is larger than the current node
                 if self.right is None:  # if the right child of that node is empty
-                    self.right = Node(insert_node) # insert your node there
+                    self.right = Node(insert_node)  # insert your node there
                 else:   # else if the right child of the node already has a value
-                    self.right.insert(insert_node) # carry out the same operation but to that right child node - basically moving down the right side of the tree
-        else: # else if there isn't a parent node
+                    self.right.insert(insert_node)  # carry out the same operation but to that right child node -
+                    # basically moving down the right side of the tree
+        else:  # else if there isn't a parent node
             self.data = insert_node  # make the node you want to insert the parent node
 
         return "{} has been inserted".format(insert_node)
@@ -47,15 +51,17 @@ class Node:
     # function to delete element in binary tree
 
     # uses recursion
-    def find_value(self,value):
+    def find_value(self, value):
         if value < self.data:  # if the value you want to find is smaller than the parent node
             if self.left is None:    # if the parent node has no left child
                 return "{} is not Found".format(value)
-            return self.left.find_value(value)  # search that node and it's child nodes to see if the value you are searching for is one of them - you're basically moving down the left side of the tree
+            return self.left.find_value(value)  # search that node and it's child nodes to see if the value you are
+            # searching for is one of them - you're basically moving down the left side of the tree
         elif value > self.data:
             if self.right is None:  # if the parent node has no right child
                 return "{} is not Found".format(value)
-            return self.right.find_value(value)  # search that node and it's child nodes to see if the value you are searching for is one of them - you're basically moving down the right side of the tree
+            return self.right.find_value(value)  # search that node and it's child nodes to see if the value you are
+            # searching for is one of them - you're basically moving down the right side of the tree
         else:   # if the parent node the value of node you are searching is equal
             return "{} is Found".format(value)
 
@@ -64,6 +70,7 @@ class Node:
         if self.left:  # if there's a left child
             self.left.print_tree()
         print(self.data)
+
         if self.right:  # if there's a right child
             self.right.print_tree()
 
@@ -79,3 +86,12 @@ root.print_tree()
 
 print(root.find_value(7))
 print(root.find_value(14))
+
+"""
+
+We created tree with 9 10 14 27 31 35 nodes
+In this tree value 7 is not there so it gives the output as 7 not found.
+14 is the left child root, hence found.
+
+
+"""
