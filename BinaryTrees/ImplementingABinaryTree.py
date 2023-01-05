f047ff3bde5b0ce8df11d81ddfd3c05bde354fac
Daniella Omokore <daniellaomokore@gmail.com>
2023-01-05 17:23:39 +0000

    commit

---
+++ BinaryTrees/ImplementingABinaryTree.py
@@ -0,0 +1,89 @@
+"""
+
+Implementing a Binary (search) Tree
+
+OPERATIONS:
+
+-Insert - compares the node value to the parent node and decided whether to add it as a left node or right node. Rule:the left node must be smaller than the root node
+If the node is greater than the parent node it is inserted as a right node, otherwise it's inserted left.
+
+-Size - returns the size of the linked list
+
+-find_value - this check if a value exists in the tree or not. When searching for a node in a tree, we transverse the node from left to right with a parent.
+
+- print_tree - prints the tree values
+"""
+
+
+class Node:
+
+    def __init__(self, data):
+        self.data = data  # node's value
+        self.left = None  # left child
+        self.right = None  # right child
+
+
+
+    # Uses recursion
+    def insert(self,insert_node):
+
+        if self.data: # if there is a parent node
+            if insert_node < self.data:  # if the node you want to insert is smaller than the current node
+                if self.left is None:  # if the left child of that node is empty
+                    self.left = Node(insert_node) # insert your node there
+                else:   # else if the left child of the node already has a value
+                    self.left.insert(insert_node) # carry out the same operation but to that left child node - basically moving down the left side of the tree
+            elif insert_node > self.data: # else if the node you want to insert is larger than the current node
+                if self.right is None:  # if the right child of that node is empty
+                    self.right = Node(insert_node) # insert your node there
+                else:   # else if the right child of the node already has a value
+                    self.right.insert(insert_node) # carry out the same operation but to that right child node - basically moving down the right side of the tree
+        else: # else if there isn't a parent node
+            self.data = insert_node  # make the node you want to insert the parent node
+
+        return "{} has been inserted".format(insert_node)
+    # function to delete the given deepest node (d_node) in binary tree
+
+    # function to delete element in binary tree
+
+    # uses recursion
+    def find_value(self,value):
+        if value < self.data:  # if the value you want to find is smaller than the parent node
+            if self.left is None:    # if the parent node has no left child
+                return "{} is not Found".format(value)
+            else:     # if the parent node has a left child
+                return self.left.find_value(value)  # search that node and it's child nodes to see if the value you are searching for is one of them - you're basically moving down the left side of the tree
+
+        elif value > self.data:
+            if self.right is None:  # if the parent node has no right child
+                return "{} is not Found".format(value)
+            else:  # if the parent node has a right child
+                return self.right.find_value(value)  # search that node and it's child nodes to see if the value you are searching for is one of them - you're basically moving down the right side of the tree
+
+        else:   # if the parent node the value of node you are searching is equal
+            return "{} is Found".format(value)
+
+    # uses recursion
+    def print_tree(self):
+        if self.left:  # if there's a left child
+            self.left.print_tree()  #
+        else:
+            print(self.data)
+
+        if self.right:  # if there's a right child
+            self.right.print_tree()
+        else:
+            print(self.data)
+
+
+root = Node(27)
+print(root.insert(14))
+print(root.insert(35))
+print(root.insert(31))
+print(root.insert(10))
+print(root.insert(9))
+
+root.print_tree()
+
+print(root.find_value(7))
+print(root.find_value(14))
--- LinkedLists/ImplementingALinkedList.py
+++ LinkedLists/ImplementingALinkedList.py
@@ -17,8 +17,8 @@ OPERATIONS:
 
 class Node:
 
-    def __init__(self, data,
-                 next_node=None):  # nextNode is initialised as default to none when a new node is created, it's assigned during the functions for different operations
+    def __init__(self, data,next_node=None):  # nextNode is initialised as default to none when a new node is created,
+        # it's assigned during the functions for different operations
         self.data = data
         self.next_node = next_node
 
@@ -76,7 +76,8 @@ class LinkedList:
 
             new_node = Node(data)
 
-            new_node.next_node = previous_node.next_node  # set the new nodes next node to point to node that is currrently at the index you want to insert at
+            new_node.next_node = previous_node.next_node  # set the new nodes next node to point to node that is
+            # currently at the index you want to insert at
             previous_node.next_node = new_node  # set the previous node of the index to point to our new node
 
             self.length_count += 1
@@ -103,7 +104,7 @@ class LinkedList:
 
         current_node = self.head  # We set the head node as our current node then iterate through the linked list until we reach the tail
 
-        while current_node.next_node.next_node != None:  # while the current node is not, node before the tail node
+        while current_node.next_node.next_node is not None:  # while the current node is not, node before the tail node
             current_node = current_node.next_node  # move to next node in the ll
 
         current_node.next_node = None  # set the node 1 before the tail node to 'None' to delete the tail node
@@ -121,7 +122,8 @@ class LinkedList:
         else:
             previous_node = self.get_node_at_index(index - 1)
 
-            previous_node.next_node = previous_node.next_node.next_node  # Make the previous node to our element point to the node after our element to delete it
+            previous_node.next_node = previous_node.next_node.next_node  # Make the previous node to our element
+            # point to the node after our element to delete it
             self.length_count -= 1
 
         return "Node at index {} deleted.".format(index)
@@ -167,7 +169,8 @@ class LinkedList:
         if self.head is None:
             return "Linked List is empty, you can't print it."
 
-        current_node = self.head  # set the head node as our current node then we will iterate through the LL to print each node starting from the head
+        current_node = self.head  # set the head node as our current node then we will iterate through the LL to
+        # print each node starting from the head
 
         while current_node is not None:  # while our current node is not the tail node
             print(current_node.data, "->", end=" ")  # print the data of the current node
@@ -181,7 +184,8 @@ class LinkedList:
             return "Linked List is empty, you can't print it."
 
         theLinkedList = []
-        current_node = self.head  # set the head node as our current node then we will iterate through the LL to print each node starting from the head
+        current_node = self.head  # set the head node as our current node then we will iterate through the LL to
+                                  # ... print each node starting from the head
 
         while current_node is not None:  # while our current node is not the tail node
             theLinkedList.append(
@@ -205,9 +209,7 @@ print("Size of linked list:", myLL.size())
 print(myLL.search(78))
 print(myLL.search(5))
 
-
 print(myLL.return_LinkedList())
-
 
 print(myLL.delete_at_index(1))
 print(myLL.return_LinkedList())
